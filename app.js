7285ba3e4e21b3a7b51d4b4a9ad04c68b209b953
jeffrey vierra <st.pious@gmail.com>
2017-08-17 09:27:33 -1000

    Added the removeChild() to shoot elements.

--- app.js
+++ app.js
@@ -7,16 +7,16 @@ const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
 const removeItemButton = document.querySelector('button.removeItemButton');
 
-listDiv.addEventListener('mouseover', (event) => {
+listDiv.addEventListener('click', (event) => {
   if (event.target.tagName == 'LI') {
-    event.target.textContent = event.target.textContent.toUpperCase();
+    let li = event.target;
+    let ul = li.parentNode;
+    ul.removeChild(li);
+    
   }
 });
-listDiv.addEventListener('mouseout', (event) => {
-  if (event.target.tagName == 'LI') {
-    event.target.textContent = event.target.textContent.toLowerCase();
-  }
-});
+
+
 
 toggleList.addEventListener('click', () => {
   if (listDiv.style.display == 'none') {
